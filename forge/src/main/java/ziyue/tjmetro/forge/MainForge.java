7c352de242bb6dd9c80ea0f227445ea77742258f
子悦解说 <ziyuecommentary@gmail.com>
2024-11-16 21:27:24 +0800

    Fixed Forge server crash

--- forge/src/main/java/ziyue/tjmetro/forge/MainForge.java
+++ forge/src/main/java/ziyue/tjmetro/forge/MainForge.java
@@ -1,17 +1,15 @@
 package ziyue.tjmetro.forge;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import org.mtr.mapping.holder.Screen;
 import ziyue.tjmetro.mod.Reference;
 import ziyue.tjmetro.mod.TianjinMetro;
 import ziyue.tjmetro.mod.TianjinMetroClient;
 import ziyue.tjmetro.mod.client.Filters;
-import ziyue.tjmetro.mod.screen.ConfigClientScreen;
 
 /**
  * @since 1.0.0-beta-1
@@ -24,23 +22,15 @@ public final class MainForge
 
     public MainForge() {
         TianjinMetro.init();
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> TianjinMetroClient::init);
-        MinecraftForge.EVENT_BUS.register(this);
-
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () ->{
-#if MC_VERSION <= "11605"
-            ModLoadingContext.get().registerExtensionPoint(net.minecraftforge.fml.ExtensionPoint.CONFIGGUIFACTORY, () -> (client, parent) -> new ConfigClientScreen(new Screen(parent)));
-#elif MC_VERSION <= "11701"
-            ModLoadingContext.get().registerExtensionPoint(net.minecraftforge.fmlclient.ConfigGuiHandler.ConfigGuiFactory.class, () -> new net.minecraftforge.fmlclient.ConfigGuiHandler.ConfigGuiFactory((client, parent) -> new ConfigClientScreen(new Screen(parent))));
-#elif MC_VERSION <= "11802"
-            ModLoadingContext.get().registerExtensionPoint(net.minecraftforge.client.ConfigGuiHandler.ConfigGuiFactory.class, () -> new net.minecraftforge.client.ConfigGuiHandler.ConfigGuiFactory((client, parent) -> new ConfigClientScreen(new Screen(parent))));
-#elif MC_VERSION <= "12004"
-            ModLoadingContext.get().registerExtensionPoint(net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory.class, () -> new net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory((client, parent) -> new ConfigClientScreen(new Screen(parent))));
-#endif
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            TianjinMetroClient.init();
+            MainForgeClient.registerConfigMenu();
         });
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public void afterRegistry(
 #if MC_VERSION <= "11701"
         net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent.Post event
