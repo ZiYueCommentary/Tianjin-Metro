d93b8a6174fc94893b0d51e7efdbefea36939e25
ZiYueCommentary <ziyuecommentary@gmail.com>
2025-11-09 10:46:11 +0800

    Updated to 1.0.2

--- fabric/src/main/java/ziyue/tjmetro/mod/Reference.java
+++ fabric/src/main/java/ziyue/tjmetro/mod/Reference.java
@@ -10,7 +10,7 @@ public interface Reference
 {
     String MOD_ID = "tjmetro";
     String NAME = "Tianjin Metro";
-    String VERSION = "1.0.1";
+    String VERSION = "1.0.2";
 
     String GITHUB_REPO = "https://github.com/ZiYueCommentary/Tianjin-Metro";
     String CONTRIBUTORS = "https://github.com/ZiYueCommentary/Tianjin-Metro/graphs/contributors";
